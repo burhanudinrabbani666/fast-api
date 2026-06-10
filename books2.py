456b133109964d8e0393b0918e2930f184db5a9b
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-06-10 22:24:17 +0700

    put request

--- books2.py
+++ books2.py
@@ -29,12 +29,31 @@ class Book:
 
 
 class Book_request(BaseModel):
-    id: Optional[int] = None
+    id: Optional[int] = Field(
+        description="Id is not needed on create", default=None
+    )
     title: str = Field(min_length=3, max_length=100)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
-    rating: float = Field(gt=1, lt=6)
+    rating: float = Field(gt=0, lt=6)
 
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "A new Book",
+                "author": "burhanudin",
+                "description": "Nice book for legend",
+                "rating": 5,
+            }
+        }
+    }
+
+
+# --------------------------------------------------------------
+#
+#
+#
+# --------------------------------------------------------------
 
 BOOKS = [
     Book(
@@ -87,10 +106,35 @@ def read_all_books():
     return BOOKS
 
 
+@app.get("/books/{book_id}")
+def read_book(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+
+
+@app.get("/books/")
+def read_book_by_rating(book_rating: int):
+    books_to_return: list[Book] = []
+
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+
+    return books_to_return
+
+
 @app.post("/create-book")
 def create_book(book_request: Book_request):
     new_book = Book(**book_request.model_dump())
     BOOKS.append(find_book_id(new_book))
+
+
+@app.put("/books/update-book")
+def update_book(book: Book_request):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
 
 
 def find_book_id(book: Book):
