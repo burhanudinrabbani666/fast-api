369b16d1d0c4db8198875d6cb6485b4be1fd20c3
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-06-10 18:35:26 +0700

    query parameters

--- books.py
+++ books.py
@@ -5,32 +5,32 @@ app = FastAPI()
 BOOKS: list[dict[str, str]] = [
     {
         "title": "Title One",
-        "author": "authoe One",
+        "author": "author One",
         "category": "science",
     },
     {
         "title": "Title Two",
-        "author": "authoe Two",
+        "author": "author Two",
         "category": "science",
     },
     {
         "title": "Title Three",
-        "author": "authoe Three",
+        "author": "author Three",
         "category": "history",
     },
     {
         "title": "Title Four",
-        "author": "authoe Four",
+        "author": "author Four",
         "category": "math",
     },
     {
         "title": "Title Five",
-        "author": "authoe Five",
+        "author": "author Five",
         "category": "math",
     },
     {
         "title": "Title Six",
-        "author": "authoe Two",
+        "author": "author Two",
         "category": "math",
     },
 ]
@@ -46,3 +46,31 @@ async def get_book_by_id(title: str):
     for book in BOOKS:
         if book.get("title", "").casefold() == title.casefold():
             return book
+
+
+@app.get("/books/", tags=["Books"])
+async def get_book_by_query(category: str):
+    books_to_return: list[dict[str, str]] = []
+    for book in BOOKS:
+        if book.get("category", "").casefold() == category.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+
+@app.get("/books/{book_author}/", tags=["Books"])
+async def get_author_category_by_query(
+    book_author: str, category: str
+):
+    books_to_return: list[dict[str, str]] = []
+
+    for book in BOOKS:
+        if (
+            book.get("author", "").casefold()
+            == book_author.casefold()
+            and book.get("category", "").casefold()
+            == category.casefold()
+        ):
+            books_to_return.append(book)
+
+    return books_to_return
