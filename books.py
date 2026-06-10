221453979293e598c440fe2a35565bce144e8183
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-06-10 17:10:11 +0700

    Inital commit

---
+++ books.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+BOOKS: list[dict[str, str]] = [
+    {
+        "title": "Title One",
+        "author": "authoe One",
+        "category": "science",
+    },
+    {
+        "title": "Title Two",
+        "author": "authoe Two",
+        "category": "science",
+    },
+    {
+        "title": "Title Three",
+        "author": "authoe Three",
+        "category": "history",
+    },
+    {
+        "title": "Title Four",
+        "author": "authoe Four",
+        "category": "math",
+    },
+    {
+        "title": "Title Five",
+        "author": "authoe Five",
+        "category": "math",
+    },
+    {
+        "title": "Title Six",
+        "author": "authoe Two",
+        "category": "math",
+    },
+]
+
+
+@app.get("/books", tags=["Books"])
+async def get_books():
+    return BOOKS
